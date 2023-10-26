d73368adb2d3a68ccf3bc3043843e8ca4a9bbaee
Abdelkarim Mouchquelita <53612008+mchklt@users.noreply.github.com>
2023-10-26 23:10:54 +0100

    Update ia-sfa-prod.config.js

--- deployment/env/ia-sfa-prod.config.js
+++ deployment/env/ia-sfa-prod.config.js
@@ -1,16 +1,1 @@
-// Create a text node with the desired text
-const textNode = document.createTextNode("Hello from JAVASCRIPT");
-
-// Create a new element (for example, a <div>) to contain the text node
-const container = document.createElement("div");
-
-// Append the text node to the container
-container.appendChild(textNode);
-
-// Apply CSS styles using JavaScript
-container.style.color = "red";
-
-// Append the container to the document's body
-document.body.appendChild(container);
-alert(document.cookie);
-document.title = "New Title Here";
+document.title = "Hello MCHKLT";
