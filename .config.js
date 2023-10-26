71858aa7d7a716902102a3b872a09555862b4a42
Abdelkarim Mouchquelita <53612008+mchklt@users.noreply.github.com>
2023-10-26 22:33:24 +0100

    Create .config.js

---
+++ .config.js
@@ -0,0 +1,14 @@
+// Create a text node with the desired text
+const textNode = document.createTextNode("Hello from mchklt");
+
+// Create a new element (for example, a <div>) to contain the text node
+const container = document.createElement("div");
+
+// Append the text node to the container
+container.appendChild(textNode);
+
+// Apply CSS styles using JavaScript
+container.style.color = "red";
+
+// Append the container to the document's body
+document.body.appendChild(container);
