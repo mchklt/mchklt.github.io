6a2e9ec7faf7ed2aa96208375afbc1513c67f44e
Abdelkarim Mouchquelita <53612008+mchklt@users.noreply.github.com>
2023-10-26 22:31:18 +0100

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,1 +1,14 @@
-alert(document.cookie);
+// Create a text node with the desired text
+const textNode = document.createTextNode("Hello from js");
+
+// Create a new element (for example, a <div>) to contain the text node
+const container = document.createElement("div");
+
+// Append the text node to the container
+container.appendChild(textNode);
+
+// Apply CSS styles using JavaScript
+container.style.color = "red";
+
+// Append the container to the document's body
+document.body.appendChild(container);
